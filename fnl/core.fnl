1adcdffb7eba663cccddc8a2c0338b4eac6604f6
a <a@as-Virtual-Machine.local>
2026-03-24 02:40:02 -0700

    Introduce recursive find-all* helper for pattern search

--- fnl/core.fnl
+++ fnl/core.fnl
@@ -1,6 +1,18 @@
 (local core (require :nfnl.core))
 
-(fn cons [x xs]
-  (core.concat [x] xs))
+(fn find-all* [s pattern hits]
+  (let [hit [(string.find s pattern)]]
+    (if (core.empty? hit)
+        hits
+        (find-all* (string.sub s (core.inc (core.first hit))) pattern
+                   (core.concat hits
+                                [(core.map (if (core.empty? hits)
+                                               core.identity
+                                               (partial +
+                                                        (core.first (core.last hits))))
+                                           hit)])))))
+
+(fn find-all [s pattern]
+  (find-all* s pattern []))
 
 {}
--- lua/core.lua
+++ lua/core.lua
@@ -1,6 +1,24 @@
 -- [nfnl] fnl/core.fnl
 local core = require("nfnl.core")
-local function cons(x, xs)
-  return core.concat({x}, xs)
+local function find_all_2a(s, pattern, hits)
+  local hit = {string.find(s, pattern)}
+  if core["empty?"](hit) then
+    return hits
+  else
+    local _1_
+    if core["empty?"](hits) then
+      _1_ = core.identity
+    else
+      local partial_2_ = core.first(core.last(hits))
+      local function _4_(...)
+        return (partial_2_ + ...)
+      end
+      _1_ = _4_
+    end
+    return find_all_2a(string.sub(s, core.inc(core.first(hit))), pattern, core.concat(hits, {core.map(_1_, hit)}))
+  end
+end
+local function find_all(s, pattern)
+  return find_all_2a(s, pattern, {})
 end
 return {}
