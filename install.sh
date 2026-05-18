db1d1991c8f30f0c9567fbab1df5da7bb68c65fd
a <a@as-Virtual-Machine.local>
2026-05-18 00:06:33 -0700

    Add checksum verification and safe extraction workflow to install script

--- install.sh
+++ install.sh
@@ -1,2 +1,6 @@
 #!/bin/sh
-curl -L https://github.com/8ta4/sentence/releases/download/v0.1.0/sentence.tar.gz | tar -xz
+set -e
+trap 'rm -f sentence.tar.gz' EXIT
+curl -L https://github.com/8ta4/sentence/releases/download/v0.1.1/sentence.tar.gz -o sentence.tar.gz
+echo '40e70f37f6cf29296adcbdf57334c658fe9a13d2fbc4ce49b91137ec96183e8d  sentence.tar.gz' | shasum -a 256 -c
+tar -xzf sentence.tar.gz
